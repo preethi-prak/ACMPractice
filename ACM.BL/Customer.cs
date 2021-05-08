617649df74c37cb6d9012a40f0b9ce24eda77995
preethi <preethi93@outlook.com>
2021-05-07 22:18:03 -0700

    #1 Commiting Customer Class and CustomerTest class

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerID { get; private set; }
+
+        public static int  InstanceCount { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullname = LastName;
+                {
+                    if (!string.IsNullOrEmpty(FirstName))
+                    {
+                        if (!string.IsNullOrEmpty(fullname))
+                        {
+                            fullname += ",";
+                        }
+                        fullname += FirstName;
+                    }
+                    return fullname;
+                }
+            }
+        }
+
+        public bool Validate ()
+        {
+            var isValid = true;
+            if (string.IsNullOrEmpty(LastName))
+                isValid = false;
+            if (string.IsNullOrEmpty(EmailAddress))
+                isValid = false;
+
+            return isValid; 
+        }
+
+        public Customer Retrieve(int customerID)
+        {
+            return new Customer();
+        }
+
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        public bool Save ()
+        {
+            return true; 
+        }
+
+    }
+}
---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,153 @@
+﻿using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Preethi";
+            customer.LastName = "Prakash";
+
+            string expectedval = "Preethi,Prakash";
+
+            //act 
+            string actualval = customer.FullName;
+
+            //assert 
+            Assert.AreEqual(expectedval,actualval);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+           
+            Customer customer = new Customer
+            {
+                LastName = "Prakash"
+
+            };
+            string expected = "Prakash";
+
+            //act 
+            string actual = customer.FullName;
+
+            //assert
+
+            Assert.AreEqual(expected, actual); 
+
+        }
+
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+
+            Customer customer = new Customer
+            {
+                FirstName = "Preethi"
+
+            };
+            string expected = "Preethi";
+
+            //act 
+            string actual = customer.FullName;
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+
+            Customer c1 = new Customer
+            {
+                FirstName = "Preethi"
+
+            };
+            Customer.InstanceCount += 1;
+
+            Customer c2 = new Customer
+            {
+                FirstName = "Deepthi"
+
+            };
+            Customer.InstanceCount += 1;
+
+
+            Customer c3 = new Customer
+            {
+                FirstName = "Arya"
+
+            };
+            Customer.InstanceCount += 1;
+
+            //act 
+        
+            //assert
+
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+
+        [TestMethod]
+        public void ValidateTest()
+        {
+            //arrange
+            Customer c = new Customer
+            {
+                
+                LastName = "Prakash",
+                EmailAddress = "Preethi93@icloud.com"
+            };
+            //act
+            var Expectedval = true;
+            var actualval = c.Validate();
+            //assert
+            Assert.AreEqual(Expectedval, actualval);
+        }
+
+        [TestMethod]
+        public void ValidateMissingLastName ()
+        {
+            //arrange
+            Customer c = new Customer
+            {
+
+                EmailAddress = "Preethi93@icloud.com"
+            };
+            //act
+            var Expectedval = false;
+            //assert
+            Assert.AreEqual(Expectedval, c.Validate());
+        }
+
+        [TestMethod]
+        public void ValidateMissingEmail()
+        {
+            //arrange
+            Customer c = new Customer
+            {
+                LastName = "Preethi"
+            };
+            //act
+            var Expectedval = false;
+            //assert
+            Assert.AreEqual(Expectedval, c.Validate());
+        }
+
+
+
+
+
+
+    }
+}
